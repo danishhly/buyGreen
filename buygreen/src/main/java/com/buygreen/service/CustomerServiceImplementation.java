4949ba747a4d3cdeb6d00ae721ea316045bf00ab
Daan153 <creative.in47@gmail.com>
2025-11-04 20:43:16 +0530

    creating a user from a google sign-in

--- buygreen/src/main/java/com/buygreen/service/CustomerServiceImplementation.java
+++ buygreen/src/main/java/com/buygreen/service/CustomerServiceImplementation.java
@@ -25,4 +25,18 @@ public class CustomerServiceImplementation implements CustomerService {
         return repo.findByEmail(email);
 
     }
+
+    @Override
+    public Customers processGoogleUser(String email, String name) {
+        Customers customer = repo.findByEmail(email);
+
+        //if customer doesn't exist, create a new one
+        if(customer == null) {
+            customer = new Customers();
+            customer.setEmail(email);
+            customer.setName(name);
+            customer.setRole("customer");
+            
+        }
+    }
 }
